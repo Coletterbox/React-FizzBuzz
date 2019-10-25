3607406b61d392eb74f8e6b64b8bd2b80b3cc3f7
Colette <colette.rs.shaw@gmail.com>
2019-10-25 18:52:48 +0100

    Create FizzBuzz using loop

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 import { getBoxStyle } from "./Spiral.js";
 
-const boxes = [
-  React.createElement("div", { style: getBoxStyle(0), key: "0" }, "1"),
-  React.createElement("div", { style: getBoxStyle(1), key: "1" }, "2"),
-  React.createElement(
-    "div",
-    { style: getBoxStyle(2), key: "2" },
-    React.createElement("strong", {}, "Fizz")
-  ),
-  React.createElement("div", { style: getBoxStyle(3), key: "3" }, "4"),
-  React.createElement(
-    "div",
-    { style: getBoxStyle(4), key: "4" },
-    React.createElement("strong", {}, "Buzz")
-  )
-];
+// const boxes = [
+//   React.createElement("div", { style: getBoxStyle(0), key: "0" }, "1"),
+//   React.createElement("div", { style: getBoxStyle(1), key: "1" }, "2"),
+//   React.createElement(
+//     "div",
+//     { style: getBoxStyle(2), key: "2" },
+//     React.createElement("strong", {}, "Fizz")
+//   ),
+//   React.createElement("div", { style: getBoxStyle(3), key: "3" }, "4"),
+//   React.createElement(
+//     "div",
+//     { style: getBoxStyle(4), key: "4" },
+//     React.createElement("strong", {}, "Buzz")
+//   )
+// ];
+
+const boxes = [];
+
+for (var i = 1; i < 16; i++) {
+  if (i % 3 !== 0 && i % 5 !== 0) {
+    boxes.push(
+      React.createElement("div", { style: getBoxStyle(i - 1), key: i - 1 }, i)
+    );
+  } else if (i % 3 !== 0) {
+    boxes.push(
+      React.createElement(
+        "div",
+        { style: getBoxStyle(i - 1), key: i - 1 },
+        React.createElement("strong", {}, "Fizz")
+      )
+    );
+  } else if (i % 5 !== 0) {
+    boxes.push(
+      React.createElement(
+        "div",
+        { style: getBoxStyle(i - 1), key: i - 1 },
+        React.createElement("strong", {}, "Buzz")
+      )
+    );
+  }
+}
 
 ReactDOM.render(
   React.createElement("div", {}, boxes),
