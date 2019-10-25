bfec4c0818540401f5a93d905f5e667c6def8378
Colette <colette.rs.shaw@gmail.com>
2019-10-25 18:40:40 +0100

    Start FizzBuzz

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-import { getBoxStyle } from './Spiral.js'
+import { getBoxStyle } from "./Spiral.js";
 
 const boxes = [
-  React.createElement('div', { style: getBoxStyle(0), key: '0' }, '1'),
-  React.createElement('div', { style: getBoxStyle(1), key: '1' }, '2'),
-  React.createElement('div', { style: getBoxStyle(2), key: '2' },
-    React.createElement('strong', {}, 'Fizz')
-  ) ,
-]
+  React.createElement("div", { style: getBoxStyle(0), key: "0" }, "1"),
+  React.createElement("div", { style: getBoxStyle(1), key: "1" }, "2"),
+  React.createElement(
+    "div",
+    { style: getBoxStyle(2), key: "2" },
+    React.createElement("strong", {}, "Fizz")
+  ),
+  React.createElement("div", { style: getBoxStyle(3), key: "3" }, "4"),
+  React.createElement(
+    "div",
+    { style: getBoxStyle(4), key: "4" },
+    React.createElement("strong", {}, "Buzz")
+  )
+];
 
 ReactDOM.render(
-  React.createElement('div', {}, boxes),
-  document.getElementById('app')
-)
+  React.createElement("div", {}, boxes),
+  document.getElementById("app")
+);
